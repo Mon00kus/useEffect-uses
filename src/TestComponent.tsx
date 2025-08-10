c8ba58fbddced71ccbfc6b4b0b392dfe6235612d
mon00kus <raulmoncadac@gmail.com>
2025-08-10 11:37:08 -0400

    First add of and endpoint to test

--- src/TestComponent.tsx
+++ src/TestComponent.tsx
@@ -1,32 +1,28 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
 
-/* type Props = {}; */
-const TestComponent = (/* props: Props */) => {
+const TestComponent = () => {
   const [count, setCount] = useState(0);
   const [darkMode, setDarkMode] = useState(false);
+  const [post, setPost] = useState([]);
 
   useEffect(() => {
-    /* document.title = `${count}`;
-    console.log("Component renderer for the first time"); */
-    /*  const myTimer = setTimeout(() => {
-        alert("Hello from testcomponent");
-      }, 10000); */
-    /*  return () => clearTimeout(myTimer); */
-    console.log(`Use effect count: ${count}`);
-    return () => console.log(`Clean Up count: ${count}`);
-  }, [count]);
+    const fectchdata = async () => {
+      const results = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+      setPost(results.data);
+      console.table(results.data);
+    };
+    fectchdata();
+  }, []);
 
-  /*  useEffect(() => {
-    localStorage.setItem("darkMode", darkMode ? "true" : "false");
-    console.log("darkMOde hook ran")
-  },[darkMode]) */
-
-  console.log("Component Render");
+  console.log("Component Rendered");
 
   return (
     <div>
       <h1>Test Components !!!</h1>
-      <h2>{count}</h2>
+      <h2>{count}</h2>      
       <button onClick={() => setCount((state) => state + 1)}>Increment</button>
       <button onClick={() => setDarkMode((state) => !state)}>
         Toggle dark mode
